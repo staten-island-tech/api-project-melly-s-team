5b91b1790096b6a735849fbf201ae28508ccadd0
robmelly <robert.melnik.5@gmail.com>
2021-05-27 00:12:34 -0400

    yo

--- sketch.js
+++ sketch.js
@@ -1,35 +1,22 @@
-var weather;
+const shortdef;
+//https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key
+const api = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
+const apiKey = "?key=dc3cdce9-25bd-4b2f-93da-ff18fe638dfd";
 
-var api = "https://api.openweathermap.org/data/2.5/weather?q=";
-var apiKey = "&APPID=001b0f58045147663b1ea518d34d88b4";
-var units = "&units=metric";
-
-var input;
+const input;
 
 function setup() {
-  createCanvas(400, 200);
+  const button = select("#submit");
+  button.mousePressed(wordAsk);
 
-  var button = select("#submit");
-  button.mousePressed(weatherAsk);
-
-  input = select("#city");
+  input = select("#word");
 }
 
-function weatherAsk() {
-  var url = api + input.value() + apiKey + units;
+function wordAsk() {
+  const url = api + input.value() + apiKey;
   loadJSON(url, gotData);
 }
 
 function gotData(data) {
-  weather = data;
-}
-
-function draw() {
-  background(0);
-  if (weather) {
-    var temp = weather.main.temp;
-    var humidity = weather.main.humidity;
-    ellipse(100, 100, temp, temp);
-    ellipse(300, 100, humidity, humidity);
-  }
+  shortdef = data;
 }
