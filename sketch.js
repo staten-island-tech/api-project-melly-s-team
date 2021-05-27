9c6bab8415daad19c753443025c0396433ac30f7
robmelly <robert.melnik.5@gmail.com>
2021-05-27 02:22:29 -0400

    yo

--- sketch.js
+++ sketch.js
@@ -1,9 +1,6 @@
-const shortdef;
 //https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key
 const api = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
 const apiKey = "?key=dc3cdce9-25bd-4b2f-93da-ff18fe638dfd";
-
-const input;
 
 function setup() {
   const button = select("#submit");
@@ -20,3 +17,44 @@ function wordAsk() {
 function gotData(data) {
   shortdef = data;
 }
+
+function test2() {
+  return Promise.resolve(Promise.resolve(shortdef));
+}
+
+console.log(test2);
+// var weather;
+
+// var api = "https://api.openweathermap.org/data/2.5/weather?q=";
+// var apiKey = "&APPID=001b0f58045147663b1ea518d34d88b4";
+// var units = "&units=metric";
+
+// var input;
+
+// function setup() {
+//   createCanvas(400, 200);
+
+//   var button = select("#submit");
+//   button.mousePressed(weatherAsk);
+
+//   input = select("#city");
+// }
+
+// function weatherAsk() {
+//   var url = api + input.value() + apiKey + units;
+//   loadJSON(url, gotData);
+// }
+
+// function gotData(data) {
+//   weather = data;
+// }
+
+// function draw() {
+//   background(0);
+//   if (weather) {
+//     var temp = weather.main.temp;
+//     var humidity = weather.main.humidity;
+//     ellipse(100, 100, temp, temp);
+//     ellipse(300, 100, humidity, humidity);
+//   }
+// }
