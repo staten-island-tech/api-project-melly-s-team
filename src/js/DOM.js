3672ceaca952450a1a630fa82176f98920172006
robmelly <robert.melnik.5@gmail.com>
2021-05-26 23:44:10 -0400

    yo

--- src/js/DOM.js
+++
@@ -1,8 +0,0 @@
-const DOMSelectors = {
-  grid: document.querySelector(".movie-grid"),
-};
-
-export { DOMSelectors };
-
-
-//use dom selectors to change the value of the word you are searching
--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import { DOMSelectors } from "./DOM";
-unirest
-  .get("https://wordsapiv1.p.mashape.com/words/soliloquy")
-  .header("X-Mashape-Key", "<required>")
-  .header("Accept", "application/json")
-  .end(function (result) {
-    console.log(result.status, result.headers, result.body);
-  });
